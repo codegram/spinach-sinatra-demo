14480233d6eebd3c5b05a6d534e246ab61435948
Benny <r3qnbenni@gmail.com>
2014-08-31 20:29:00 +0200

    require minitest/spec instead of minitest/autorun

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,6 @@
 require_relative '../../app'
 
 require 'spinach/capybara'
-require 'minitest/autorun'
+require 'minitest/spec'
 Spinach::FeatureSteps.send(:include, Spinach::FeatureSteps::Capybara)
 Capybara.app = Sinatra::Application
